c041d1de417f57859a898ec028e01802d50f5f75
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-16 15:35:24 +0530

    FINAL COMMIT

--- src/helpInMaths.java
+++ src/helpInMaths.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class helpInMaths {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        char ch[] = s.toCharArray();
+        for (int j = 0; j < ch.length
+                ; j = j + 2) {
+            for (int i = j; i < ch.length; i = i + 2) {
+                if (ch[j] > ch[i]) {
+
+                    char temp = ch[i];
+                    ch[i] = ch[j];
+                    ch[j] = temp;
+                }
+            }
+
+        }
+        for (int k = 0; k < ch.length; k++) {
+            System.out.print(ch[k]);
+        }
 
     }
 
